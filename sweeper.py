2489b69ebf87c9344ccdde50b185f53370305a16
rafmillan <rafael.k.millan@gmail.com>
2019-11-10 16:47:18 -0800

    selecting empty cell now also exposes adjacent empty cells

--- sweeper.py
+++ sweeper.py
@@ -44,6 +44,7 @@ def printBoard(A, stdscr, currRow, currCol):
 		 	stdscr.attroff(curses.color_pair(9))
 		 	stdscr.attroff(curses.color_pair(10))
 	stdscr.refresh()
+
 def printHiddenBoard(A, stdscr):
 	maxY, maxX = stdscr.getmaxyx()
 
@@ -82,12 +83,14 @@ def printHiddenBoard(A, stdscr):
 		 	stdscr.attroff(curses.color_pair(10))
 
 	stdscr.refresh()
+
 def printArgs(stdscr):
 	stdscr.addstr (0, 0, "Rows: %s" % (sys.argv[1]))
 	stdscr.addstr (1, 0, "Columns: %s" % (sys.argv[2]))
 	stdscr.addstr (2, 0, "Bombs: %s" % (sys.argv[3]))
 	if len(sys.argv) == 5:
 		stdscr.addstr (3, 0, "Seed: %s" % (sys.argv[4]))
+
 def setCurses(stdscr):
 	curses.curs_set(0)
 	curses.noecho()
@@ -103,11 +106,13 @@ def setCurses(stdscr):
 	curses.init_pair(9, curses.COLOR_WHITE, 0)
 	curses.init_pair(10, curses.COLOR_WHITE, curses.COLOR_RED)
 	stdscr.keypad(True)
+
 def isMine(row, col, board):
 	if board[row][col] == '[X]':
 		return True
 	else:
 		return False
+
 def placeBombs(board):
 	row = random.randint(0, len(board)-1)
 	col = random.randint(0, len(board[0])-1)
@@ -118,6 +123,7 @@ def placeBombs(board):
 		currentRow[col] = '[X]'
 	else:
 		placeBombs(board)
+
 def fillBoard(board):
 	bombCount = 0
 	for i, row in enumerate(board):
@@ -166,11 +172,55 @@ def fillBoard(board):
 				str = "[%i]" % bombCount
 				board[i][j] = str
 				bombCount = 0
+
 def checkBomb(board, myBoard, row, col):
 	myBoard[row][col] = board[row][col]
+	#check 8 adjacent cells and count numbombs
+	#North [row-1][col]
+	if not row == 0:
+		if not isMine(row-1, col, board):
+			myBoard[row-1][col] = board[row-1][col]
+			
+	#North East [row-1][col+1]
+	if not row == 0 and not col == len(board[row])-1:
+		if not isMine(row-1, col+1, board):
+			myBoard[row-1][col+1] = board[row-1][col+1]
+
+	#East [row][col+1]
+	if not col == len(board[row])-1:
+		if not isMine(row, col+1, board):
+			myBoard[row][col+1] = board[row][col+1]
+
+	#South East [row+1][col+1]
+	if not row == len(board)-1 and not col == len(board[col])-1:
+		if not isMine(row+1, col+1, board):
+			myBoard[row+1][col+1] = board[row+1][col+1]
+
+	#South [row+1][col]
+	if not row == len(board)-1:
+		if not isMine(row+1, col, board):
+			myBoard[row+1][col] = board[row+1][col]
+
+	#South West [row+1][col-1]
+	if not row == len(board)-1 and not col == 0:
+		if not isMine(row+1, col-1, board):
+			myBoard[row+1][col-1] = board[row+1][col-1]
+
+	#West [row][col-1]
+	if not col == 0:
+		if not isMine(row, col-1, board):
+			myBoard[row][col-1] = board[row][col-1]
+
+	#North West [row-1][col-1]
+	if not row == 0 and not col == 0:
+		if not isMine(row-1, col-1, board):
+			myBoard[row-1][col-1] = board[row-1][col-1]
+
 	if isMine(row, col, board):
 		return True;
-	else: return False
+	else: 
+		return False
+
 def GameOver(stdscr):
 	stdscr.erase()
 	label  = ['GAME OVER.', 'press [Q] to quit...']
@@ -181,7 +231,15 @@ def GameOver(stdscr):
 		y = 1+i
 		stdscr.addstr(y, x, row)
 
+def winGameOver(stdscr):
+	stdscr.erase()
+	label  = ['YOU WIN.', 'press [Q] to quit...']
+	maxY, maxX = stdscr.getmaxyx()
 
+	for i, row in enumerate(label):
+		x = maxX//2 - len(row)//2
+		y = 1+i
+		stdscr.addstr(y, x, row)
 
 def main(stdscr):
 
@@ -191,9 +249,13 @@ def main(stdscr):
 	rows = int(sys.argv[1])
 	cols = int(sys.argv[2])
 	numBombs = int(sys.argv[3])
+	#spotsLeft = (rows * cols) - numBombs
+	spotsLeft = 0
 
 	bombProb = numBombs/(rows*cols)
 	stdscr.addstr(4,0,"bombProb: %.2f" % (bombProb))
+	stdscr.addstr(5,0,"Spots Left: %i" % (spotsLeft))
+	stdscr.refresh()
 
 	if len(sys.argv) == 5:
 		seed = int(sys.argv[4])
@@ -229,33 +291,34 @@ def main(stdscr):
 
 	running = True
 	gameOver = False
+	win = False
+	keyLock = False
 	while running:
-
 		printArgs(stdscr)
 		printBoard(myBoard, stdscr, currRow, currCol)
 		printHiddenBoard(board, stdscr)
 
 		key = stdscr.getch()
 
-		if key == curses.KEY_UP and not gameOver:
+		if key == curses.KEY_UP and not keyLock:
 			if currRow <= 0:
 				currRow = currRow
 			else: 
 				currRow -= 1
 			currCell = myBoard[currRow][currCol]
-		elif key == curses.KEY_DOWN and not gameOver:
+		elif key == curses.KEY_DOWN and not keyLock:
 			if currRow >= rows-1:
 				currRow = currRow
 			else: 
 				currRow += 1
 			currCell = myBoard[currRow][currCol]
-		elif key == curses.KEY_LEFT and not gameOver:
+		elif key == curses.KEY_LEFT and not keyLock:
 			if currCol <= 0:
 				currCol = currCol
 			else: 
 				currCol -= 1
 			currCell = myBoard[currRow][currCol]
-		elif key == curses.KEY_RIGHT and not gameOver:
+		elif key == curses.KEY_RIGHT and not keyLock:
 			if currCol >= cols-1:
 				currCol = currCol
 			else: 
@@ -263,7 +326,7 @@ def main(stdscr):
 			currCell = myBoard[currRow][currCol]
 		elif key == ord('q') or key == ord('Q'):
 			exit()
-		elif key == ord('z') or key == ord('Z') and not gameOver:
+		elif key == ord('z') or key == ord('Z') and not keyLock:
 			if checkBomb(board, myBoard, currRow, currCol):
 				gameOver = True
 
